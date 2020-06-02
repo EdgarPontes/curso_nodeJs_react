3573609e74080de99a212542eb10e6189f413a81
Edgar Pontes <edgarponte@gmail.com>
2020-06-01 22:29:50 -0300

    Model, Repository, Model and Controller

---
+++ api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/cursoreact', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const UserModel = require('./src/models/user');
+var USERS = [
+    { 'id': 1, 'username': 'edgar', 'password': '123456' },
+    { 'id': 2, 'username': 'pontes', 'password': '123456' },
+];
+
+var HELLO = [
+    { 'msg': 'Hello Express' }
+];
+
+function getHello() {
+    return HELLO;
+}
+
+function getUsers() {
+    return USERS;
+}
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send(getHello());
+});
+
+const userRouter = require('./src/routes/user-route');
+
+app.use('/user', userRouter);
+
+app.listen(4000, function () {
+    console.log('Hello Express Listen on Port 4000');
+});
